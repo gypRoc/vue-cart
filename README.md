4e6fe7687d5421d619a52ba25330104e5a55c395
路坎坷 <386986374@qq.com>
2020-12-02 15:16:55 +0800

    效果初步完成

---
+++ README.md
@@ -0,0 +1,25 @@
+# vue-cart1.
+
+## vue 实现一个卡片动效
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <app-bar title="INTEREST" left="search" right="crosshairs" />
+    <tab-list></tab-list>
+    <card-list />
+    <card-detail></card-detail>
+  </div>
+</template>
+
+<script>
+import AppBar from '@/components/AppBar.vue'
+import TabList from '@/components/TabList.vue'
+import CardList from '@/components/CardList.vue'
+import CardDetail from '@/components/CardDetail.vue'
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  // name: 'Home',
+  components: {
+    AppBar,
+    TabList,
+    CardList,
+    CardDetail
+  }
+}
+</script>
+
+<style lang="scss">
+.home {
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+  color: white;
+  background-image: linear-gradient(to top, #99acbe 5%, #a8b3bf 20%, #cfd7dd);
+}
+</style>
